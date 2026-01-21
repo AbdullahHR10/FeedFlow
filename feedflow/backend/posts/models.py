81d9327dad4095d10510e762ebe70198866cf725
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-21 14:58:30 +0200

    feat: create posts app with initial Post model

---
+++ feedflow/backend/posts/models.py
@@ -0,0 +1,19 @@
+"""Defines the Post class."""
+from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    """Represents a post in the application."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author} - {self.created_at}"
