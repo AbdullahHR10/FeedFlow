4284f4fb2b58a749077286f876a5f3327a7dce7b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-23 11:56:03 +0200

    feat: add PostBaseModel, Reaction and Comment models

--- feedflow/backend/posts/models.py
+++ feedflow/backend/posts/models.py
@@ -1,10 +1,21 @@
-"""Defines the Post class."""
+"""Defines the Post, Reaction, Comment classs."""
 from django.db import models
 from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
-class Post(models.Model):
+
+class PostBaseModel(models.Model):
+    """Defines common attributes for other classes."""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+
+
+class Post(PostBaseModel):
     """Represents a post in the application."""
     author = models.ForeignKey(
         User,
@@ -13,8 +24,52 @@ class Post(models.Model):
     )
     is_edited = models.BooleanField(default=False)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.author} - {self.created_at}"
+        return f"Post #{self.id} by {self.author}"
+
+
+class ReactionType(models.TextChoices):
+    LIKE = "like", "Like"
+    LOVE = "love", "Love"
+    LAUGH = "laugh", "Haha"
+    WOW = "wow", "Wow"
+    ANGRY = "angry", "Angry"
+
+
+class Reaction(PostBaseModel):
+    """Represents a reaction in the application."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="reactions"
+    )
+    type = models.CharField(
+        max_length=10,
+        choices=ReactionType.choices,
+        default=ReactionType.LIKE
+    )
+
+    class Meta:
+        unique_together = ("user", "post")
+
+    def __str__(self):
+        return f"{self.user} - reaction on post #{self.post_id}"
+
+
+class Comment(PostBaseModel):
+    """Represents a comment in the application."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"{self.user} - comment on post #{self.post_id}"
