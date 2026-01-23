366cc6398eae28a2364e311c588c6c4a53a70ad5
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-23 14:35:44 +0200

    Add MediaType and PostMedia classes

--- feedflow/backend/posts/models.py
+++ feedflow/backend/posts/models.py
@@ -1,8 +1,15 @@
-"""Defines the Post, Reaction, Comment classs."""
+"""Defines the Post, PostMedia, Reaction and Comment classs."""
 from django.db import models
 from django.conf import settings
+from uuid import uuid4
+import os
 
 User = settings.AUTH_USER_MODEL
+
+
+def media_upload_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    return f"media/users/{instance.post.author.id}/{uuid4()}{ext}"
 
 
 class PostBaseModel(models.Model):
@@ -23,13 +30,37 @@ class Post(PostBaseModel):
         related_name="posts"
     )
     is_edited = models.BooleanField(default=False)
-    content = models.TextField()
+    content = models.TextField(blank=True)
 
     def __str__(self):
         return f"Post #{self.id} by {self.author}"
 
 
+class MediaType(models.TextChoices):
+    """Defines allowed values for media fields."""
+    IMAGE = "image", "Image"
+    VIDEO = "video", "Video"
+
+
+class PostMedia(PostBaseModel):
+    """Represents media attached to a post."""
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="media"
+    )
+    file = models.FileField(upload_to=media_upload_path)
+    type = models.CharField(
+        max_length=10,
+        choices=MediaType.choices
+    )
+    order = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order", "created_at"]
+
 class ReactionType(models.TextChoices):
+    """Defines allowed values for reaction fields"""
     LIKE = "like", "Like"
     LOVE = "love", "Love"
     LAUGH = "laugh", "Haha"
