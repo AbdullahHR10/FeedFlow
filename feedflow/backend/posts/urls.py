3adf1312b267cad857a24f7eb3ef74b6f7fa5a89
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-02-04 14:25:30 +0200

    feat: nest reactions API in posts router

--- feedflow/backend/posts/urls.py
+++ feedflow/backend/posts/urls.py
@@ -1,6 +1,6 @@
 """URL routing for post-related API endpoints."""
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, ReactionViewSet
 
 
 router = DefaultRouter()
@@ -8,5 +8,6 @@ router.register("posts", PostViewSet, basename="post")
 
 posts_router = NestedDefaultRouter(router, "posts", lookup="post")
 posts_router.register("comments", CommentViewSet, basename="post-comments")
+posts_router.register("reactions", ReactionViewSet, basename="post-reactions")
 
 urlpatterns = router.urls + posts_router.urls
