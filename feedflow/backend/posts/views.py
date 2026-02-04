038bfca376318c0103fc18ce17a7eca63837a2e8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-02-04 10:40:15 +0200

    feat: add CommentViewset with permissions and optimized queryset

--- feedflow/backend/posts/views.py
+++ feedflow/backend/posts/views.py
@@ -7,9 +7,9 @@ Includes:
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from .models import Post
-from .serializers import PostSerializer, PostCreateSerializer
-from .permissions import IsPostAuthor
+from .models import Post, Comment
+from .serializers import PostSerializer, PostCreateSerializer, CommentSerializer
+from .permissions import IsPostAuthor, IsCommentAuthor
 from .utils import with_post_stats
 
 
@@ -18,7 +18,7 @@ class PostViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsPostAuthor]
 
     def get_queryset(self):
-        """Returns the query set."""
+        """Returns the posts query set."""
         return (
             with_post_stats(
                 Post.objects
@@ -37,3 +37,21 @@ class PostViewSet(ModelViewSet):
     def perform_update(self, serializer):
         """Sets is_edited to True on update."""
         serializer.save(is_edited=True)
+
+
+class CommentViewSet(ModelViewSet):
+    """Defines API endpoints for comments."""
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsCommentAuthor]
+
+    def get_queryset(self):
+        """Return comments for a specific post."""
+        post_id = self.kwargs.get("post_pk")
+        return Comment.objects.filter(post_id=post_id).select_related("user")
+
+    def perform_create(self, serializer):
+        """Attach authenticated user and post to the comment."""
+        serializer.save(
+            user=self.request.user,
+            post_id=self.kwargs.get("post_pk")
+        )
