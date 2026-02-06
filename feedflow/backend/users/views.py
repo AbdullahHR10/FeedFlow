94ff6df5ff2238cf828e892be3d0687f0580ddc0
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-02-06 07:33:37 +0200

    feat add UserViewSet with permissions and optimized queryset

--- feedflow/backend/users/views.py
+++ feedflow/backend/users/views.py
@@ -2,17 +2,44 @@
 API endpoints for users.
 
 Includes:
-- UserProfileViewSet: API endpoints for users profiles.
+- UserViewSet: API endpoints for users.
+- UserProfileViewSet: API endpoints for users public profiles.
 """
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.decorators import action
+
 from django.shortcuts import get_list_or_404
-from .serializers import PublicProfileSerializer
-from .models import Profile
+from .serializers import UserSerializer, PublicProfileSerializer
+from posts.serializers import PostSerializer
+from .models import User, Profile
+from posts.models import Post
 from .utils import with_profile_stats
+from posts.utils import with_post_stats
+
+
+class UserViewSet(ReadOnlyModelViewSet):
+    """Define API endpoints for users."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(detail=True, methods=["get"])
+    def posts(self, request, pk=None):
+        """Return posts written by user."""
+        queryset = with_post_stats(
+            Post.objects.filter(author_id=pk)
+        )
+
+        page = self.paginate_queryset(queryset)
+        serializer = PostSerializer(
+            page,
+            many=True,
+            context={"request": request}
+        )
+        return self.get_paginated_response(serializer.data)
 
 
 class UserProfileViewSet(ReadOnlyModelViewSet):
-    """Define API endpoints for users profiles."""
+    """Define API endpoints for users public profiles."""
     serializer_class = PublicProfileSerializer
     queryset = Profile.objects.select_related_("user")
 
