e6781ba000f995b07d188a27c52f61264a13385e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-02-06 07:39:45 +0200

    feat add FollowViewSet with permissions and optimized queryset

--- feedflow/backend/users/views.py
+++ feedflow/backend/users/views.py
@@ -4,14 +4,22 @@ API endpoints for users.
 Includes:
 - UserViewSet: API endpoints for users.
 - UserProfileViewSet: API endpoints for users public profiles.
+- FollowViewSet: API endpoints for follow.
 """
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from django.shortcuts import get_list_or_404
-from .serializers import UserSerializer, PublicProfileSerializer
+from .serializers import (
+    UserSerializer,
+    PublicProfileSerializer,
+    FollowSerializer
+)
 from posts.serializers import PostSerializer
-from .models import User, Profile
+from .models import User, Profile, Follow
 from posts.models import Post
 from .utils import with_profile_stats
 from posts.utils import with_post_stats
@@ -49,3 +57,30 @@ class UserProfileViewSet(ReadOnlyModelViewSet):
             super().get_queryset(),
             viewer=self.request.user
         )
+
+
+class FollowViewSet(GenericViewSet):
+    """Define API endpoints for follow."""
+    permission_classes = [IsAuthenticated]
+    serializer_class = FollowSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Create a follow between users."""
+        serializer = self.get_serializer(
+            data={"following": kwargs["user_pk"]},
+            context={"request": request}
+        )
+        serializer.is_valid(raise_expection=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        """Delete a follow between users."""
+        Follow.objects.filter(
+            follower=request.user,
+            follower=request.user,
+            following_id=kwargs["user_pk"]
+        ).delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
