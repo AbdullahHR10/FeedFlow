bdc8b76d97b0417fcab8ccd0fd5a6c656bc7346a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-02-06 09:44:47 +0200

    feat: add router-based URLS for users API

---
+++ feedflow/backend/users/urls.py
@@ -0,0 +1,29 @@
+"""URL routing for user-related API endpoints."""
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from django.urls import path, include
+
+from .views import (
+    UserViewSet,
+    UserProfileViewSet,
+    FollowViewSet
+)
+
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
+
+urlpatterns = router.urls + [
+    path(
+        "users/<int:pk>/profile/",
+        UserProfileViewSet.as_view({"get": "retrieve"}),
+        name="user-profile"
+    ),
+    path(
+        "users/<int:user_pk>/follow/",
+        FollowViewSet.as_view({
+            "post": "create",
+            "delete": "destroy"
+        }),
+        name="user-follow"
+    )
+]
