7a88e57b65ae02a75f9fd265659c0ccaed3472dd
AbdullahHR10 <abdullah.h.ragab1@gmail.com>
2026-02-07 16:52:04 +0200

    feat: add JWT authentication

--- feedflow/backend/core/urls.py
+++ feedflow/backend/core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include("users.auth_urls")),
     path('api/v1/', include("posts.urls")),
     path("api/v1/", include("users.urls"))
 ]
--- feedflow/backend/users/urls.py
+++ feedflow/backend/users/urls.py
@@ -1,6 +1,6 @@
 """URL routing for user-related API endpoints."""
-from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
-from django.urls import path, include
+from rest_framework_nested.routers import DefaultRouter
+from django.urls import path
 
 from .views import (
     UserViewSet,
