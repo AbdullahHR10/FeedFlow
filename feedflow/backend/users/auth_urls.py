f0b026185528d78534166e2720e16bb17bc0e590
AbdullahHR10 <abdullah.h.ragab1@gmail.com>
2026-02-08 14:21:14 +0200

    feat: add signup url

--- feedflow/backend/users/auth_urls.py
+++ feedflow/backend/users/auth_urls.py
@@ -4,9 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import SignUpView
 
 
 urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
